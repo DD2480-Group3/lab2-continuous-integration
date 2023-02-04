10c6d7ff9eda66d4a200182ab437e9845d33f338
Louise <louiseti@kth.se>
2023-02-04 17:15:51 +0100

    test: added tests for mavenbuild.java
    Added tests for mavenbuild.java.
    Two tests that compilation works correctly, and two tests that testing
    works correctly using mavenbuild.java
    
    Fixes #16

---
+++ dummyTestBuildFalse/src/main/java/DummyBuildFalse.java
@@ -0,0 +1,6 @@
+public class DummyTrue {
+    public static void main(String[] args) {
+        System.out.println("hello")
+    }
+
+}
---
+++ dummyTestTestTrue/src/main/java/DummyTestTrue.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTestTrue {
+
+    @Test
+    public void testTrue(){
+        Assert.assertTrue(true);
+    }
+}
