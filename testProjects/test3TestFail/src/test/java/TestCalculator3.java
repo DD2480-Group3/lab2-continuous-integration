0a6b42a9783e283dc81e12b45f01615fec7aaa10
Louise <louiseti@kth.se>
2023-02-05 11:01:33 +0100

    fix: fixed bugs in testcases
    Fixed so all testcases works and passes.
    Fixes #24

--- testProjects/test3TestFail/src/test/java/TestCalculator3.java
+++ testProjects/test3TestFail/src/test/java/TestCalculator3.java
@@ -1,28 +1,20 @@
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Tests for Calculator.java class
  * */
-public class CalculatorTester {
-
-    Calculator calculator;
-
-    @BeforeEach
-    void setUp() {
-        calculator = new Calculator();
-    }
+public class TestCalculator3 {
 
     /**
      * Tests sum method
      * */
     @Test
     public void testSum() {
-
-        assertEquals(10, calculator.add(5, 5));
+        Calculator calculator = new Calculator();
+        assertEquals(100, calculator.add(5, 5));
 
     }
 
