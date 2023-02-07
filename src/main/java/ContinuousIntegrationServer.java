de49a2bf93005a586a895a2d657d6c2f03bb3821
Fredrik Jansson <fja2@kth.se>
2023-02-07 16:45:59 +0100

    feat: ability to view files in web interface

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -12,7 +12,9 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jgit.api.Git;
 
+//Imports for the web interface
 import java.io.File;
+import java.nio.file.*;
 
 /**
  * Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -80,14 +82,39 @@ public class ContinuousIntegrationServer extends AbstractHandler {
             String historyDirectoryPath = "web/history"; //Set the path to the history directory
             File historyDirectory = new File(historyDirectoryPath);
             File[] historyFiles = historyDirectory.listFiles();
-
+            
+            response.getWriter().println("<h1>CI history explorer</h1><br><br>");
             for (File file : historyFiles) { //For each file in the history directory
                 if (file.isFile()) { //If the file is a file and not a directory
                     //Basic html code is used to display the files
                     response.getWriter().println("<a href=/history/" + file.getName() + ">" + file.getName() + "</a><br><br>");
                 }
             }
-        } else {
+            
+        } else if (target.startsWith("/history/")) { //If the user navigates to /history/...
+            String historyDirectoryPath = "web/history"; //Set the path to the history directory
+            File historyDirectory = new File(historyDirectoryPath);
+            File[] historyFiles = historyDirectory.listFiles();
+
+            String fileName = target.substring(9); //Get the file name from the url (starts at position 9), will be used to check if the file exists
+
+            //If the file exists, display it otherwise display an error message
+            if (new File(historyDirectoryPath + "/" + fileName).exists()) { //Check if the file exists
+                response.getWriter().println("<h1>CI history explorer</h1>");
+                response.getWriter().println("<h2>Currently at: " + fileName + "</h2>"); //Print the file name
+                response.getWriter().println("<h2><a href=/history>Back</a></h2><br><br>");
+                //Read the file into a string and display it
+                String fileContent2 = new String(Files.readAllBytes(Paths.get(historyDirectoryPath + "/" + fileName)));
+                response.getWriter().println(fileContent2);
+            } else { //If the file does not exist display an error message
+                response.getWriter().println("<h1>CI history explorer</h1>");
+                response.getWriter().println("<h2><a href=/history>Back</a></h2><br><br>");
+                response.getWriter().println("<b>ERROR: File not found</b>");
+            }
+
+        }
+
+         else {
             response.getWriter().println("CI job done");
         }
 
